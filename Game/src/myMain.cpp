90eb539cb6cf0aa8d38a8b8183223fe24b988926
Malan-Tai <malantai99@gmail.com>
2021-05-23 16:51:29 +0200

    Replaced window pointer with instance

--- Game/src/myMain.cpp
+++ Game/src/myMain.cpp
@@ -1,27 +1,26 @@
 #include "myMain.h"
 #include <iostream>
-#include <pugixml.hpp>
 #include <SFML/Graphics.hpp>
 
 
 int myMain()
 {
-    auto window = std::make_unique<sf::RenderWindow>(sf::VideoMode(1000, 1000), "Hollow Lens");
+    auto window = sf::RenderWindow{ sf::VideoMode(1000, 1000), "Hollow Lens" };
 
-    while (window->isOpen())
+    while (window.isOpen())
     {
         sf::Event event;
-        while (window->pollEvent(event))
+        while (window.pollEvent(event))
         {
-            if (event.type == sf::Event::Closed) window->close();
+            if (event.type == sf::Event::Closed) window.close();
             else if (event.type == sf::Event::KeyPressed)
             {
-                if (sf::Keyboard::isKeyPressed(sf::Keyboard::Escape)) window->close();
+                if (sf::Keyboard::isKeyPressed(sf::Keyboard::Escape)) window.close();
             }
         }
 
-        window->clear(sf::Color::White);
-        window->display();
+        window.clear(sf::Color::White);
+        window.display();
     }
 
     return 0;
