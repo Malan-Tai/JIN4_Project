0a6f9ffa4ec4f758561684d2866f0823f0f7f081
Malan-Tai <malantai99@gmail.com>
2021-05-23 14:39:20 +0200

    Base game loop

--- Game/src/myMain.cpp
+++ Game/src/myMain.cpp
@@ -1,11 +1,28 @@
 #include "myMain.h"
 #include <iostream>
 #include <pugixml.hpp>
+#include <SFML/Graphics.hpp>
 
 
 int myMain()
 {
-    std::cout << "Hello World!" << std::endl;
+    auto window = std::make_unique<sf::RenderWindow>(sf::VideoMode(1000, 1000), "Hollow Lens");
+
+    while (window->isOpen())
+    {
+        sf::Event event;
+        while (window->pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed) window->close();
+            else if (event.type == sf::Event::KeyPressed)
+            {
+                if (sf::Keyboard::isKeyPressed(sf::Keyboard::Escape)) window->close();
+            }
+        }
+
+        window->clear(sf::Color::White);
+        window->display();
+    }
 
     return 0;
 }
