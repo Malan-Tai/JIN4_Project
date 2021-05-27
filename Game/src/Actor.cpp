36e795aa5c8c7800272487c5f979eb3e1b5be946
Malan-Tai <malantai99@gmail.com>
2021-05-27 17:57:27 +0200

    Added FSM

--- Game/src/Actor.cpp
+++ Game/src/Actor.cpp
@@ -3,11 +3,34 @@
 Actor::Actor(TextureHolder const& holder) : holder(holder)
 {
 	sprite.setTexture(holder.get(texture::ID::mainCharacter_idle), true);
+
+	std::vector<M::Trans> transitions
+	{
+		// from state     , to state      , trigger,    guard   , action
+		{ States::Ground, States::Fall, Triggers::Jump, nullptr, nullptr },
+	};
+
+	machine.add_transitions(transitions);
 }
 
 void Actor::update(sf::Time const& elapsed)
 {
-	velocity += 200 * elapsed.asSeconds() * sf::Vector2f(0, 1); // gravity
+	States state = machine.state();
+	moveControl = 1;
+
+	float gravity = 0;
+	if (state == States::Fall)
+	{
+		gravity = 200;
+		moveControl = 0.5f;
+	}
+	else if (state == States::FastFall)
+	{
+		gravity = 400;
+		moveControl = 0;
+	}
+	velocity += gravity * elapsed.asSeconds() * sf::Vector2f(0, 1); // gravity
+
 	coords += elapsed.asSeconds() * velocity;
 
 	sprite.setPosition(coords);
@@ -17,6 +40,8 @@ void Actor::jump()
 {
 	float vx = velocity.x / 2;
 	velocity = sf::Vector2f{ vx, -500 };
+
+	machine.execute(Triggers::Jump);
 }
 
 void Actor::setVelocity(sf::Vector2f unitVelocity)
@@ -27,7 +52,7 @@ void Actor::setVelocity(sf::Vector2f unitVelocity)
 void Actor::setHorizontalVelocity(float dx)
 {
 	float dy = velocity.y;
-	velocity = sf::Vector2f(dx * speed, dy);
+	velocity = sf::Vector2f(dx * speed * moveControl, dy);
 }
 
 void Actor::draw(sf::RenderWindow& window) const
--- Game/src/Actor.h
+++ Game/src/Actor.h
@@ -1,6 +1,36 @@
 #pragma once
 #include "Prototype.h"
 #include "AnimHandler.h"
+#include "fsm.h"
+
+enum class States
+{
+	Ground,
+	Fall,
+	FastFall,
+	FallAttack,
+	Roll,
+	Sprint,
+	LightAttack,
+	HeavyAttack,
+	Dead
+};
+
+enum class Triggers
+{
+	Jump,
+	Land,
+	Fall,
+	PressDown,
+	ReleaseDown,
+	PressRoll,
+	EndRoll,
+	LightAttack,
+	EndLightAttack,
+	HeavyAttack,
+	EndHeavyAttack,
+	Die
+};
 
 class Actor : public Prototype
 {
@@ -18,6 +48,7 @@ public:
 
 private:
 	float speed = 300;
+	float moveControl = 1;
 
 	sf::Vector2f velocity{ 0, 0 };
 	sf::Vector2f coords{ 500, 500 };
@@ -25,4 +56,8 @@ private:
 	sf::Sprite sprite;
 
 	TextureHolder const& holder;
+
+	FSM::Fsm<States, States::Ground, Triggers> machine;
 };
+
+using M = FSM::Fsm<States, States::Ground, Triggers>;
